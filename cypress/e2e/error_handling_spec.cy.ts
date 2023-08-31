6d7a9c3febd718588076cf8dc7038a417929bff2
Laura Guerra <l.garciaguerra1@gmail.com>
2023-08-31 09:43:39 -0400

    Add saved projects fixture and switch intercept to custom command

--- cypress/e2e/error_handling_spec.cy.ts
+++ cypress/e2e/error_handling_spec.cy.ts
@@ -1,10 +1,7 @@
 describe('error handling spec', () => {
 
   beforeEach(() => { 
-    // cy.stubSingleFetch()
-    cy.intercept('GET', 'https://8c3a0c1f-6f70-4e2c-82aa-c8e6de99ae51.mock.pstmn.io/api/v1/users/1/projects', {
-      statusCode: 200, 
-    })
+    cy.stubSingleFetch('users/1/projects', "savedProjects", 200)
   })
   it('should display a message and button for bad routes', () => {
     cy.checkBadRoute('/')
