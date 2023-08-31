cf5bf1314a567e772598b204f33178660f606d39
Fernando Robles <fernandorobles97@yahoo.com>
2023-08-31 15:33:21 -0500

    Remove unnecessary imports; refactor to es6

--- src/Components/Results/DemoCarousel.tsx
+++ src/Components/Results/DemoCarousel.tsx
@@ -1,19 +1,16 @@
 import "react-responsive-carousel/lib/styles/carousel.min.css";
-import picture from '../../images/add.png'
-var React = require('react');
-var ReactDOM = require('react-dom');
-var Carousel = require('react-responsive-carousel').Carousel;
+import { Carousel } from "react-responsive-carousel";
 
 interface CarouselProps {
   videos:  JSX.Element[] | undefined
 }
 
-var DemoCarousel = ({videos}: CarouselProps) => {
-        return (
-            <Carousel showArrows={true} height='500px'>
-                {videos}
-            </Carousel>
-        );
-    }
+const DemoCarousel = ({videos}: CarouselProps) => {
+  return (
+    <Carousel showArrows={true}>
+      {videos}
+    </Carousel>
+  );
+}
 
 export default DemoCarousel
