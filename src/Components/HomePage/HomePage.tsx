c8c966e1d42447da2d4795bc5ad84631930e535d
“Saki <sakisandra.c@gmail.com>
2023-08-31 20:28:50 -0600

    Make project title smaller on mobile

--- src/Components/HomePage/HomePage.tsx
+++ src/Components/HomePage/HomePage.tsx
@@ -10,14 +10,14 @@ const HomePage = ({ smallScreen }: { smallScreen: boolean }) => {
       <section className='light-section'>
         <img className='light' src={homepageLogo} alt='notepad with lightbulb logo for app'/>
         {!smallScreen && <h1 className='homepage-title'>Welcome to the</h1>}
-        <img src={projectPlanner} className='title-img' alt='project planner text' />
+       {!smallScreen && <img src={projectPlanner} className='title-img' alt='project planner text' />}
       </section>
       <section className='popup'>
-        {smallScreen && <h1 className='homepage-title'>Welcome to Project Planner</h1>}
+        {smallScreen && <h1 className='homepage-title'> Welcome to Project Planner</h1>}
         <div className='project-info-container'>
-          <p className='project-info'>
+          {!smallScreen && <p className='project-info'>
             An application designed to streamline the project planning process for developers by leveraging advanced AI technology to craft comprehensive project plans and generate innovative ideas!
-          </p>
+          </p>}
         </div>
         <section className='popup-buttons-container'>
           <Link to='/tutorial' className='popup-button' >View Tutorial</Link>
