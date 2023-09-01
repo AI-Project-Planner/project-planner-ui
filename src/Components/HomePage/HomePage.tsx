1d3272b55697d3304de71e00ecd3031161f4bd54
“Saki <sakisandra.c@gmail.com>
2023-08-31 19:20:00 -0600

    Add project description

--- src/Components/HomePage/HomePage.tsx
+++ src/Components/HomePage/HomePage.tsx
@@ -14,6 +14,11 @@ const HomePage = ({ smallScreen }: { smallScreen: boolean }) => {
       </section>
       <section className='popup'>
         {smallScreen && <h1 className='homepage-title'>Welcome to Project Planner</h1>}
+        <div className='project-info-container'>
+          <p className='project-info'>
+            An application designed to streamline the project planning process for developers by leveraging advanced AI technology to craft comprehensive project plans and generate innovative ideas!
+          </p>
+        </div>
         <section className='popup-buttons-container'>
           <button className='popup-button' >View Tutorial</button>
           <Link className='popup-button' to='/form'>Generate New Project Plan</Link>
