2950cb58b344a9eb10b56180ee2e4c55b2706f66
“Saki <sakisandra.c@gmail.com>
2023-08-31 18:42:47 -0600

    Replace lightbulb logo with notebook sticker

--- src/Components/HomePage/HomePage.tsx
+++ src/Components/HomePage/HomePage.tsx
@@ -1,16 +1,19 @@
 import { Link } from 'react-router-dom'
 import light from '../../images/lightbulb.png'
-import './HomePage.css'
+import './HomePage.css';
+import homepageLogo from '../../images/drawing.png';
+import projectPlanner from '../../images/project-planner.png';
 
 const HomePage = ({ smallScreen }: { smallScreen: boolean }) => {
   return (
     <section className='home'>
       <section className='light-section'>
-        <img className='light' src={light} alt='lightbulb with leaves inside'/>
-        {!smallScreen && <h1>Welcome to Project Planner</h1>}
+        <img className='light' src={homepageLogo} alt='notepad with lightbulb logo for app'/>
+        {!smallScreen && <h1 className='homepage-title'>Welcome to the</h1>}
+        <img src={projectPlanner} className='title-img' alt='project planner text' />
       </section>
       <section className='popup'>
-        {smallScreen && <h1>Welcome to Project Planner</h1>}
+        {smallScreen && <h1 className='homepage-title'>Welcome to Project Planner</h1>}
         <section className='popup-buttons-container'>
           <button className='popup-button' >View Tutorial</button>
           <Link className='popup-button' to='/form'>Generate New Project Plan</Link>
