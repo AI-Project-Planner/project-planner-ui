e81118f481587a273e791587017d3f071914a2c0
Fernando Robles <fernandorobles97@yahoo.com>
2023-09-01 10:59:07 -0500

    Add image for no results page

--- src/Components/NoResults/NoResults.tsx
+++ src/Components/NoResults/NoResults.tsx
@@ -1,9 +1,11 @@
 import { Link} from 'react-router-dom'
+import emptySpace from '../../images/woman.png'
 import './NoResults.css'
 
 const NoResults = () => {
   return (
     <section className="no-results-container">
+      <img src={emptySpace} alt='person meditating in space'></img>
       <h2>Just like the void of space, there's nothing to see here!</h2>
       <Link to='/form'>To Form</Link>
     </section>
