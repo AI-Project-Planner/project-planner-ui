72f44f130ebd096662127080f26dd5558cf4f9b6
“Saki <sakisandra.c@gmail.com>
2023-08-31 22:52:00 -0600

    Add custom command to before each to load homepage

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -7,8 +7,7 @@ Cypress.Commands.add('loadHomepage', () => {
     "https://8c3a0c1f-6f70-4e2c-82aa-c8e6de99ae51.mock.pstmn.io/api/v1/users/1/projects",
     {
       statusCode: 200,
-      body: { },
-      //what is this request getting?? come back and fill out later once we have real data
+      fixture: 'savedProjects'
     }
   )
 });
