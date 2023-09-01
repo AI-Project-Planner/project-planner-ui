6eefa920e41e0366ad22ba8c6ec4ecf2e17d5c77
Fernando Robles <fernandorobles97@yahoo.com>
2023-09-01 14:48:29 -0500

    Intercept initial request of saved projects; set up first test with complete form command

--- cypress/e2e/results-spec.cy.ts
+++ cypress/e2e/results-spec.cy.ts
@@ -1,3 +1,13 @@
 describe('results spec', () => {
-  
+  beforeEach(() => {
+    cy.intercept('GET', 'https://8c3a0c1f-6f70-4e2c-82aa-c8e6de99ae51.mock.pstmn.io/api/v1/users/1/projects', {
+      status: 200,
+      fixture: 'savedProjects'
+    }).as('getEpisodes')
+    cy.visit('http://localhost:3000/form')
+  })
+
+  it('should allow a user to see a results page after filling out a form', () => {
+    cy.completeForm()
+  })
 })
