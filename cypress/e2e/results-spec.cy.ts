b9bc7cc8365ff8725cc5056a48b035d6ab459aa4
Fernando Robles <fernandorobles97@yahoo.com>
2023-09-01 15:17:27 -0500

    Add intercept for POST api call when submit button is clicked

--- cypress/e2e/results-spec.cy.ts
+++ cypress/e2e/results-spec.cy.ts
@@ -5,9 +5,14 @@ describe('results spec', () => {
       fixture: 'savedProjects'
     }).as('getEpisodes')
     cy.visit('http://localhost:3000/form')
+    cy.intercept('POST', 'https://8c3a0c1f-6f70-4e2c-82aa-c8e6de99ae51.mock.pstmn.io/api/v1/users/1/projects/', {
+      status: 200,
+      fixture: 'unSaved1'
+    })
   })
 
   it('should allow a user to see a results page after filling out a form', () => {
     cy.completeForm()
+    cy.get('.form-button').click()
   })
 })
