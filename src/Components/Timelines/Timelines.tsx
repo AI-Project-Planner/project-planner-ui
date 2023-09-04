add270f899810c57f4cb160a09c777199bfb2ce0
Laura Guerra <l.garciaguerra1@gmail.com>
2023-09-04 19:04:38 -0400

    Add slideshow feature to timeline

--- src/Components/Timelines/Timelines.tsx
+++ src/Components/Timelines/Timelines.tsx
@@ -4,6 +4,8 @@ import doubleleft from '../../images/keyboarddoubleleft.png'
 import left from '../../images/keyboardleft.png'
 import right from '../../images/keyboardright.png'
 import doubleright from '../../images/keyboarddoubleright.png'
+import play from '../../images/play_arrow_FILL0_wght400_GRAD0_opsz24.png'
+import pause from '../../images/stop_FILL0_wght400_GRAD0_opsz24.png'
 
 interface TimelineProps {
   steps: string[]
@@ -11,7 +13,14 @@ interface TimelineProps {
 
 const Timelines = ({steps}: TimelineProps) => {
   const [currentStep, setCurrentStep] = useState<string>(steps[0]);
-  const [stepNum, setStepNum] = useState(0)
+  const [stepNum, setStepNum] = useState(0);
+  const [slideShow, setSlideShow] = useState(false);
+  let timer:string | number | NodeJS.Timer | undefined
+  useEffect(() => {
+    console.log(timer)
+    
+  }, [timer])
+  
   
   const showStep = (step: string, num: number) => {
     setCurrentStep(step)
@@ -47,11 +56,41 @@ const Timelines = ({steps}: TimelineProps) => {
 
   const timelineDetails = steps.map(step => {
     return (
-      <div className={`${currentStep === step? 'timeline-detail' : 'hidden'}`}>
-        <p>{step}</p>
+      <div style={{ width: '100vw', display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent:'center'}} key={step}>
+        <div className={`${currentStep === step? 'timeline-detail' : 'hidden'}`}>
+          <p>{step}</p>
+        </div>
+        {slideShow && <div className={`${currentStep === step ? 'slideshow-line' : 'hidden'}`}></div>}
       </div>
     )
   })
+
+  useEffect(() => {
+    if (slideShow) {
+      steps.forEach((step, i) => {
+        if (!i) {
+          setStepNum(i)
+        } else {
+          timer = setTimeout(() => {
+            setStepNum(i)
+            if (i === steps.length - 1) {
+              setSlideShow(false)
+              // clearInterval(timer)
+            }
+          }, 1500 * i)
+        }
+      })
+    }
+  }, [slideShow])
+
+  const playOrPauseSlideShow = () => {
+    if (slideShow) {
+      clearTimeout(timer)
+      setSlideShow(false)
+    } else {
+      setSlideShow(true)
+    }
+  }
 
   return (
     <div className='timeline-main'>
@@ -64,12 +103,10 @@ const Timelines = ({steps}: TimelineProps) => {
         <button onClick={() => setStepNum(0)}><img src={doubleleft} alt='timeline control button to first step' /></button>
         <button onClick={goToPrevStep}><img src={left} alt='timeline control button to previous step' /></button>
         <button onClick={goToNextStep}><img src={right} alt='timeline control button to next step' /></button>
-        <button onClick={() => setStepNum(steps.length-1)}><img src={doubleright} alt='timeline control button to last step'/></button>
+        <button onClick={() => setStepNum(steps.length - 1)}><img src={doubleright} alt='timeline control button to last step' /></button>
+        <button onClick={playOrPauseSlideShow}><img src={slideShow? pause : play} alt='timeline control button to last step'/></button>
       </section>
-      {/* <div className='timeline-detail'>
-        <p>{currentStep}</p>
-      </div> */}
-       {timelineDetails}
+      {timelineDetails}
     </div>
   )
 }
