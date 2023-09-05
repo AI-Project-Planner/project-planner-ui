c2b93437fb80755d547361a10e7c194d8c26ac0b
Laura Guerra <l.garciaguerra1@gmail.com>
2023-09-04 20:02:54 -0400

    Fix timeline slideshow buig

--- src/Components/Timelines/Timelines.tsx
+++ src/Components/Timelines/Timelines.tsx
@@ -6,6 +6,8 @@ import right from '../../images/keyboardright.png'
 import doubleright from '../../images/keyboarddoubleright.png'
 import play from '../../images/play_arrow_FILL0_wght400_GRAD0_opsz24.png'
 import pause from '../../images/stop_FILL0_wght400_GRAD0_opsz24.png'
+import { useRef } from 'react';
+// import { clearTimeout } from 'timers';
 
 interface TimelineProps {
   steps: string[]
@@ -15,24 +17,28 @@ const Timelines = ({steps}: TimelineProps) => {
   const [currentStep, setCurrentStep] = useState<string>(steps[0]);
   const [stepNum, setStepNum] = useState(0);
   const [slideShow, setSlideShow] = useState(false);
-  let timer:string | number | NodeJS.Timer | undefined
+  let timer = useRef<NodeJS.Timeout | number>(0)
   useEffect(() => {
-    console.log(timer)
+    console.log(1, timer.current)
     
   }, [timer])
   
   
   const showStep = (step: string, num: number) => {
-    setCurrentStep(step)
-    setStepNum(num)
+    if (!slideShow) { 
+      setCurrentStep(step)
+      setStepNum(num)
+    }
   }
   
   const goToNextStep = () => {
-    if(stepNum !== steps.length - 1) setStepNum(prev => prev + 1)
+    if (!slideShow && stepNum !== steps.length - 1) {
+      setStepNum(prev => prev + 1)
+    }
   }
   
   const goToPrevStep = () => {
-    if(stepNum !== 0) setStepNum(prev => prev - 1)
+    if(!slideShow && stepNum !== 0) setStepNum(prev => prev - 1)
   }
   
   useEffect(() => {
@@ -71,25 +77,20 @@ const Timelines = ({steps}: TimelineProps) => {
         if (!i) {
           setStepNum(i)
         } else {
-          timer = setTimeout(() => {
+          timer.current = setTimeout(() => {
             setStepNum(i)
             if (i === steps.length - 1) {
               setSlideShow(false)
-              // clearInterval(timer)
             }
           }, 1500 * i)
+          console.log(2, timer.current)
         }
       })
     }
   }, [slideShow])
 
-  const playOrPauseSlideShow = () => {
-    if (slideShow) {
-      clearTimeout(timer)
-      setSlideShow(false)
-    } else {
-      setSlideShow(true)
-    }
+  const playSlideShow = () => {
+    setSlideShow(true)
   }
 
   return (
@@ -100,11 +101,18 @@ const Timelines = ({steps}: TimelineProps) => {
       </div>
       <div className='timeline-line'></div>
       <section className='controls'>
-        <button onClick={() => setStepNum(0)}><img src={doubleleft} alt='timeline control button to first step' /></button>
-        <button onClick={goToPrevStep}><img src={left} alt='timeline control button to previous step' /></button>
-        <button onClick={goToNextStep}><img src={right} alt='timeline control button to next step' /></button>
-        <button onClick={() => setStepNum(steps.length - 1)}><img src={doubleright} alt='timeline control button to last step' /></button>
-        <button onClick={playOrPauseSlideShow}><img src={slideShow? pause : play} alt='timeline control button to last step'/></button>
+        {
+          !slideShow
+            ? 
+            <>
+              <button onClick={() => { if (!slideShow) setStepNum(0) }}><img src={doubleleft} alt='timeline control button to first step' /></button>
+              <button onClick={goToPrevStep}><img src={left} alt='timeline control button to previous step' /></button>
+              <button onClick={goToNextStep}><img src={right} alt='timeline control button to next step' /></button>
+              <button onClick={() => { if (!slideShow) setStepNum(steps.length - 1) }}><img src={doubleright} alt='timeline control button to last step' /></button>
+              <button onClick={playSlideShow}><img src={play} alt='timeline control button to last step'/></button>
+            </>
+            : <p>Step {stepNum + 1}</p>
+        }
       </section>
       {timelineDetails}
     </div>
