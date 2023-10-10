eec611fc7574cf85c9350ed6325dc361e6c7b879
Sandra Chatphatthanasiri <sakisandra.c@gmail.com>
2023-10-10 15:24:04 -0600

    Wrap app in oauth

--- src/index.tsx
+++ src/index.tsx
@@ -3,14 +3,17 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './Components/App/App';
 import { BrowserRouter } from 'react-router-dom';
+import { GoogleOAuthProvider } from '@react-oauth/google';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <BrowserRouter>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </BrowserRouter>
+  <GoogleOAuthProvider clientId="530282796412-t7lmaaof23343sd8nc3flqege614asb3.apps.googleusercontent.com">
+    <BrowserRouter>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </BrowserRouter>
+  </GoogleOAuthProvider>
 );
